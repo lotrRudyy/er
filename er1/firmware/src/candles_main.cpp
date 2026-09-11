edfaac0185a5f9c1822681b6b2c0f72d15afca31
Rudy <35534361+lotrRudyy@users.noreply.github.com>
2026-09-11 15:17:11 +0200

    version patches noch mqtt reset und esp restart

--- er1/firmware/src/candles_main.cpp
+++ er1/firmware/src/candles_main.cpp
@@ -9,7 +9,7 @@
 using namespace Core;
 
 static const char* NODE_ID = "candles";
-static const char* FW_VERSION = "45";
+static const char* FW_VERSION = "48";
 static const char* FW_DESC = "candles mqtt recovery 15s ethernet 60s esp";
 
 static const uint8_t MAC_ADDR[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x58};
